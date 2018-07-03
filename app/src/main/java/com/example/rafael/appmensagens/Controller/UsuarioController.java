1a9fe43ff5dbf1bdc0c349572e03eaae5717e657
Matheus Rodrigues <mm.matheusrodrigues@gmail.com>
2018-07-03 18:41:46 -0300

    Task - Add Contato 100%

--- app/src/main/java/com/example/rafael/appmensagens/Controller/UsuarioController.java
+++ app/src/main/java/com/example/rafael/appmensagens/Controller/UsuarioController.java
@@ -5,6 +5,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.example.rafael.appmensagens.Model.Contato;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioController{
     private static UsuarioController instance;
@@ -41,12 +47,52 @@ public class UsuarioController{
             SharedPreferences pref = context.getSharedPreferences("UserPreferences", 0);
 
             SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+
             editor.putInt("id_user", id_user); // Storing integer
             editor.commit();
         }
         catch (Exception ex){
 
         }
+    }
+
+    public void SalvarContatos(String id_user, List<Contato> contatos, Context context){
+        try {
+            SharedPreferences pref = context.getSharedPreferences(id_user, 0);
+            SharedPreferences.Editor editor = pref.edit();
+
+            editor.clear();
+            Gson gson = new Gson();
+            String jsonText = gson.toJson(contatos);
+            editor.putString("contatos", jsonText);
+            editor.commit();
+        }
+        catch (Exception ex){
+
+        }
+    }
+    public List<Contato> BuscarContatos(String id_user,  Context context){
+        List<Contato> contatos = null;
+        try {
+            SharedPreferences pref = context.getSharedPreferences(id_user, 0);
+            SharedPreferences.Editor editor = pref.edit();
+
+            Gson gson = new Gson();
+            String jsonText = pref.getString("contatos", null);
+
+            Type listType = new TypeToken<ArrayList<Contato>>(){}.getType();
+
+            contatos = gson.fromJson(jsonText, listType);
+        }
+        catch (Exception ex){
+
+        }
+
+        if(contatos == null)
+            contatos = new ArrayList<>();
+
+        return contatos;
     }
 
     public Contato getUsuarioLogado() {
